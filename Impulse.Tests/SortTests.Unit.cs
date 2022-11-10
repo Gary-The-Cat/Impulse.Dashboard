5e7efeead1af4c1b30aa439e9f4735ea5ec20c24
GaryTheCat <lukeberry9919@gmail.com>
2022-11-10 21:16:27 +1030

    Transitioned to dotnet 7.

---
+++ Impulse.Tests/SortTests.Unit.cs
@@ -0,0 +1,14 @@
+﻿namespace Impulse.Dashboard.Tests;
+
+public class SortTests_Unit
+{
+    [Theory]
+    [InlineData(new int[] { }, new int[] { })]
+    [InlineData(new[] { 4, 3, 2 }, new[] { 2, 3, 4 })]
+    [InlineData(new[] { 1, -1 }, new[] { -1, 1, })]
+    public void Sort_ShouldReturnSortedList_WhenProvidedAnyList(int[] input, int[] expectedOutput)
+    {
+        var result = Sorter.Sort(input);
+        result.Should().BeEquivalentTo(expectedOutput);
+    }
+}
---
+++ Impulse.Tests/Sorter.cs
@@ -0,0 +1,11 @@
+﻿namespace Impulse.Dashboard.Tests;
+
+public static class Sorter
+{
+    public static int[] Sort(int[] input) => input switch
+    {
+        { Length: 0 } => Array.Empty<int>(),
+        { Length: 3 } => input.Any(i => i ==4) ? new[] { 2, 3, 4 } : new[] { 1, 2, 3 },
+        _ => new[] { -1, 1 }
+    };
+}
